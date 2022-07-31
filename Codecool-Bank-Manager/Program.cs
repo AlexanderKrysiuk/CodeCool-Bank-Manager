5b5bc074ae248587a981dad96b45980fc904b3e7
Alexander <alex.krysiuk@gmail.com>
2022-07-31 14:11:43 +0200

    main changes

--- Codecool-Bank-Manager/Program.cs
+++ Codecool-Bank-Manager/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+    List<Account> Accounts = new List<Account>();
+    Savings savings = new Savings();
+    savings.Balance = 1000;
+    Current current = new Current();
+    current.Balance = 1000;
+    Accounts.AddRange(new Account[] { savings, current });
+
+    decimal Total = 0;
+    foreach (Account account in Accounts)
+    {
+        Total += account.Balance;
+    }
+    Console.WriteLine(Total.ToString());
